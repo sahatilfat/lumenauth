9f2cd26ce4dcc0fb7fcf30c68e14dfa950895947
sahatilfat <sahatilfat@gmail.com>
2021-12-06 12:19:17 +0700

    Membuat Fungsi Register pada Framework Lumen

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,37 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Client\Request;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash; //untuk melakukan hashing
+use App\Models\User; //memanggil model user
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $password = Hash::make($request->input('password'));
+
+        $register =  User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => $password,
+        ]);
+
+        if ($register) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Register Success',
+                'data' => $register,
+            ], 201);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Register Fail!',
+                'data' => '',
+            ], 400);
+        }
     }
 
     public function login(Request $request)
