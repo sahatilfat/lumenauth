a3635ff9c617ac6af5767e2bc66d6a9167e404b5
sahatilfat <sahatilfat@gmail.com>
2021-12-06 11:10:42 +0700

    Menggunakan Authentication pada Framework Lumen

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Client\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+    }
+
+    public function login(Request $request)
+    {
+    }
+}
